cc649a631fab1870960e612c5adf8af671e96c8b
Saul Blanco Tejero <alu15067@iesgalileo.es>
2019-06-03 20:45:06 +0200

    Desarrollo de la clase Partido

--- LaLigaSantander/src/laligasantander/Partido.java
+++ LaLigaSantander/src/laligasantander/Partido.java
@@ -10,5 +10,15 @@ package laligasantander;
  * @author tarod
  */
 public class Partido {
-    
+    private final Equipo equipoLocal;
+    private final Integer resultadoLocal;
+    private final Equipo equipoContrincante;
+    private final Integer resultadoContrincante;
+
+    public Partido(Equipo equipoLocal, Integer resultadoLocal, Equipo equipoContrincante, Integer resultadoContrincante) {
+        this.equipoLocal = equipoLocal;
+        this.resultadoLocal = resultadoLocal;
+        this.equipoContrincante = equipoContrincante;
+        this.resultadoContrincante = resultadoContrincante;
+    }
 }
